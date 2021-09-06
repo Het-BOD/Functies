a0b3218607186989b7dd91fcc589c4a0f7e206ac
Rik Scharn <rik_scharn@hotmail.com>
2021-09-06 11:25:10 +0200

    Update Geo_functies.R

--- Geo_functies.R
+++ Geo_functies.R
@@ -1,13 +1,13 @@
 
 # Geo Fucties -------------------------------------------------------------
-
+### Functie die RDS omzet in coords voor leaflet op basis van losse x en y coords.
+## De projectie is aan te passen als je weet welke crs hier voor nodig is
 Swap <- function(df, X, Y, projection = CRS("+init=epsg:28992"), into = CRS("+proj=longlat +datum=WGS84")){
   require(sp) ; require(sf) ; require(magrittr)
   options(digits = 14)
   X <- substitute(X)
   Y <- substitute(Y)
-  # coords <-  df %>% 
-  #   select(X, Y)
+
   df %<>% filter(!is.na(df[X]))
   XCOORD <- sapply(df[X], gsub, pattern = ",", replacement = ".") %>% as.numeric()
   YCOORD <- sapply(df[Y], gsub, pattern = ",", replacement = ".") %>% as.numeric()
@@ -15,9 +15,7 @@ Swap <- function(df, X, Y, projection = CRS("+init=epsg:28992"), into = CRS("+pr
   coordinates(coords) <- coords
   proj4string(coords) <- projection
   new_coords <- spTransform(coords, into)
-  # df$X %<>% new_coords@coords[,1]
-  # df$Y %<>% new_coords@coords[,2]
-  # return(df)
+  
   `<-`(df[X],new_coords@coords[,1])
   `<-`(df[Y],new_coords@coords[,2])
   return(df)
